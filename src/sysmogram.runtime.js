a56b5692968a40aa9b95235c0cbd9ee8ba1edee2
Himanshu Vinchhi <hjvinchhi@gmail.com>
2020-03-01 18:25:29 +0530

    adding demo files

---
+++ src/sysmogram.runtime.js
@@ -0,0 +1,8 @@
+this.sys = new sysmogram();
+
+this.sys.getObservable().subscribe(function(data){
+  // this is the callback function - whenever button is being clicked 
+  // obervable is going to send us the data here.   
+  document.getElementById('myText').value = data.startDate;
+});
+
